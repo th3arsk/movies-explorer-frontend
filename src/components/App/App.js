341778b4045ec0d27b0b7e00ea22c21b990fba4e
Alexander Rasyuk <th3arsk@yandex.ru>
2024-01-14 14:38:41 +0300

    level-3 reactored (6)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,101 +1,112 @@
-import './App.css'
-import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import "./App.css";
+import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
-import CurrentUserContext from '../../contexts/CurrentUserContext';
-import ProtectedRoute from '../../utils/ProtectedRoute';
+import CurrentUserContext from "../../contexts/CurrentUserContext";
+import ProtectedRoute from "../../utils/ProtectedRoute";
 
-import { signIn, signUp, getUserInfo, editUserInfo } from '../../utils/MainApi';
-import { getSavedMovies, getMovies } from '../../utils/MoviesApi';
-import useFilter from '../../utils/useFilter';
+import { signIn, signUp, getUserInfo, editUserInfo } from "../../utils/MainApi";
+import { getSavedMovies, getMovies } from "../../utils/MoviesApi";
+import useFilter from "../../utils/useFilter";
 
-import Main from '../Main/Main';
-import Movies from '../Movies/Movies';
-import SavedMovies from '../SavedMovies/SavedMovies';
-import Register from '../Register/Register';
-import Login from '../Login/Login';
-import Profile from '../Profile/Profile';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import Error from '../Error/Error';
+import Main from "../Main/Main";
+import Movies from "../Movies/Movies";
+import SavedMovies from "../SavedMovies/SavedMovies";
+import Register from "../Register/Register";
+import Login from "../Login/Login";
+import Profile from "../Profile/Profile";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Error from "../Error/Error";
 
 function App() {
-  const [ currentUser, setCurrentUser ] = React.useState({ });
-  const [ loggedIn, setLoggedIn ] = React.useState(false);
+  const [currentUser, setCurrentUser] = React.useState({});
+  const [loggedIn, setLoggedIn] = React.useState(false);
 
-  const [ savedMovies, setSavedMovies ] = React.useState([]);
-  const [ movies, setMovies ] =  React.useState([]);
-  const [ isLoading, setLoading ] = React.useState(false);
+  const [savedMovies, setSavedMovies] = React.useState([]);
+  const [movies, setMovies] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(false);
 
   const navigate = useNavigate();
-  const filteredMovies = useFilter(movies, localStorage.getItem("moviesSearch"), localStorage.getItem("moviesCheckbox"));
-  const filteredSavedMovies = useFilter(savedMovies, localStorage.getItem("savedMoviesSearch"), localStorage.getItem("savedMoviesCheckbox"));
+  const filteredMovies = useFilter(
+    movies,
+    localStorage.getItem("moviesSearch"),
+    localStorage.getItem("moviesCheckbox")
+  );
+  const filteredSavedMovies = useFilter(
+    savedMovies,
+    localStorage.getItem("savedMoviesSearch"),
+    localStorage.getItem("savedMoviesCheckbox")
+  );
 
-  const token = localStorage.getItem('jwt');
+  const token = localStorage.getItem("jwt");
 
-  React.useEffect(() => {  
-    if (token) {
-      setLoggedIn(true)
-    } else {
-      navigate("/signin", { replace: true })
-      setLoggedIn(false)
-    }
+  React.useEffect(() => {
+    console.log(loggedIn);
+    // if (token) {
+    //   setLoggedIn(true);
+    // } else {
+    //   navigate("/signin", { replace: true });
+    //   setLoggedIn(false);
+    // }
   }, [loggedIn]);
-  
-  React.useEffect(()=> { 
+
+  React.useEffect(() => {
     getSavedMovies()
       .then((res) => {
         setSavedMovies(res);
         localStorage.setItem("savedMovies", JSON.stringify(res));
       })
-      .catch(err => console.log(`Ошибка.....: ${err}`))
-    }, [loggedIn, savedMovies])
+      .catch((err) => console.log(`Ошибка.....: ${err}`));
+  }, [loggedIn]);
 
-  React.useEffect(()=>{
-    setLoading(true)
+  React.useEffect(() => {
+    setLoading(true);
     getMovies()
       .then((res) => {
-        setMovies(res)
+        setMovies(res);
       })
-      .catch(err => console.log(`Ошибка.....: ${err}`))
-      .finally(() => {setLoading(false)})
-    }, [loggedIn])
+      .catch((err) => console.log(`Ошибка.....: ${err}`))
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [loggedIn]);
 
-  React.useEffect(() => { 
+  React.useEffect(() => {
     getUserInfo()
-     .then((res) => {
+      .then((res) => {
         setCurrentUser(res);
       })
-      .catch(err => console.log(`Ошибка.....: ${err}`));
-    }, [loggedIn]);
+      .catch((err) => console.log(`Ошибка.....: ${err}`));
+  }, [loggedIn]);
 
-  function login( email, password ) {
-    signIn( email, password ) 
-    .then((res) => {
-      if (res.token) {
-        localStorage.setItem("moviesSearch", "")
-        localStorage.setItem("savedMoviesSearch", "")
-        localStorage.setItem("moviesCheckbox", false)
-        localStorage.setItem("savedMoviesCheckbox", false)
-        setLoggedIn(true)
-        navigate("/movies", { replace: true })
-      } else {
-        Promise.reject(`Ошибка: ${res.status}`)
-      }
-    })
-    .catch(err => console.log(`Ошибка.....: ${err}`));
+  function login(email, password) {
+    signIn(email, password)
+      .then((res) => {
+        if (res.token) {
+          localStorage.setItem("moviesSearch", "");
+          localStorage.setItem("savedMoviesSearch", "");
+          localStorage.setItem("moviesCheckbox", false);
+          localStorage.setItem("savedMoviesCheckbox", false);
+          setLoggedIn(true);
+          navigate("/movies", { replace: true });
+        } else {
+          Promise.reject(`Ошибка: ${res.status}`);
+        }
+      })
+      .catch((err) => console.log(`Ошибка.....: ${err}`));
   }
 
   function auth(name, email, password) {
-    signUp(name, email, password )
-    .then((res) => {
-      if (res.name) {  
-        login( email, password )
-      } else {
-        Promise.reject(`Ошибка: ${res.status}`)
-      }
-    })
-    .catch(err => console.log(`Ошибка.....: ${err}`));
+    signUp(name, email, password)
+      .then((res) => {
+        if (res.name) {
+          login(email, password);
+        } else {
+          Promise.reject(`Ошибка: ${res.status}`);
+        }
+      })
+      .catch((err) => console.log(`Ошибка.....: ${err}`));
   }
 
   function logout() {
@@ -104,106 +115,106 @@ function App() {
     localStorage.clear();
   }
 
-  function edit( name, email ) {
+  function edit(name, email) {
     editUserInfo(name, email)
-    .then((res) => {
-      setCurrentUser(res);
-      alert("Сохранено");
-    })
-    .catch(err => console.log(`Ошибка.....: ${err}`));
+      .then((res) => {
+        setCurrentUser(res);
+        alert("Сохранено");
+      })
+      .catch((err) => console.log(`Ошибка.....: ${err}`));
   }
 
   return (
-    <div className='App'>
-      <CurrentUserContext.Provider value={currentUser} >
-        <Routes >
-          <Route path='/signin' element={
-            <ProtectedRoute 
-              token={!token} 
-              element={<Login onLogin={login} />}
-            />
-          }/>
-            <Route path='/signup' element={
-            <ProtectedRoute 
-              token={!token} 
-              element={<Register onAuth={auth} />}
-            />
-          }/>
-          <Route path='*' element={<Error />}/>
-          <Route path='/' element={ 
-            <>
-              <Header 
-                isLoggedIn={loggedIn} 
-                light={false} 
-                moviesOpened={false}
-                savedMoviesOpened={false} 
-                mainOpened={true}
+    <div className="App">
+      <CurrentUserContext.Provider value={currentUser}>
+        <Routes>
+          <Route path="/signin" element={<Login onLogin={login} />} />
+          <Route path="/signup" element={<Register onAuth={auth} />} />
+          <Route path="*" element={<Error />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Header
+                  isLoggedIn={loggedIn}
+                  light={false}
+                  moviesOpened={false}
+                  savedMoviesOpened={false}
+                  mainOpened={true}
+                />
+                <Main />
+                <Footer />
+              </>
+            }
+          />
+          <Route
+            path="/movies"
+            element={
+              <ProtectedRoute
+                token={token}
+                element={
+                  <>
+                    <Header
+                      isLoggedIn={loggedIn}
+                      light={true}
+                      moviesOpened={true}
+                      savedMoviesOpened={false}
+                      mainOpened={false}
+                    />
+                    <Movies
+                      movies={filteredMovies}
+                      savedMovies={savedMovies}
+                      isLoading={isLoading}
+                    />
+                    <Footer />
+                  </>
+                }
               />
-              <Main /> 
-              <Footer />     
-            </>    
-          }/>
-         <Route path="/movies" element={
-           <ProtectedRoute 
-              token={token} 
-              element={
-              <>
-                <Header 
-                  isLoggedIn={loggedIn} 
-                  light={true} 
-                  moviesOpened={true}
-                  savedMoviesOpened={false} 
-                  mainOpened={false}
-                />
-                <Movies
-                  movies={filteredMovies}
-                  savedMovies={savedMovies}
-                  isLoading={isLoading}
-                />
-                <Footer />     
-              </>
-            }/>  
-          }/> 
-          <Route path='/saved-movies' element={
-            <ProtectedRoute 
-              token={token}
-              element={
-              <>
-                <Header 
-                  isLoggedIn={loggedIn} 
-                  light={true} 
-                  moviesOpened={false}
-                  savedMoviesOpened={true} 
-                  mainOpened={false}
-                />
-                <SavedMovies
-                  movies={filteredSavedMovies}
-                />
-                <Footer />     
-              </>
-            } />  
-          } /> 
-          <Route path='/profile' element={
-            <ProtectedRoute 
-              token={token}
-              element={
-              <>
-                <Header 
-                  isLoggedIn={loggedIn} 
-                  light={true}
-                  moviesOpened={false}
-                  savedMoviesOpened={false} 
-                  mainOpened={false}
-                />
-                <Profile 
-                  onExit={logout}
-                  onEdit={edit}
-                />
-              </>
-            }/>  
-          }/> 
-        </Routes >
-      </CurrentUserContext.Provider >
+            }
+          />
+          <Route
+            path="/saved-movies"
+            element={
+              <ProtectedRoute
+                token={token}
+                element={
+                  <>
+                    <Header
+                      isLoggedIn={loggedIn}
+                      light={true}
+                      moviesOpened={false}
+                      savedMoviesOpened={true}
+                      mainOpened={false}
+                    />
+                    <SavedMovies movies={filteredSavedMovies} />
+                    <Footer />
+                  </>
+                }
+              />
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute
+                token={token}
+                element={
+                  <>
+                    <Header
+                      isLoggedIn={loggedIn}
+                      light={true}
+                      moviesOpened={false}
+                      savedMoviesOpened={false}
+                      mainOpened={false}
+                    />
+                    <Profile onExit={logout} onEdit={edit} />
+                  </>
+                }
+              />
+            }
+          />
+        </Routes>
+      </CurrentUserContext.Provider>
     </div>
   );
 }
